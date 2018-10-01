7cd49e6ec1e57c5ddf3da9d672509b2114720723
Student <Student@165.24.36.162>
2018-10-01 14:26:57 -0700

    Code4life,goofy names, and guessing pi.

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -11,7 +11,7 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-		String x = JOptionPane.showInputDialog("waTs Ya nAm?");
+		String x = JOptionPane.showInputDialog("What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
 System.out.println(x.toUpperCase());
@@ -22,16 +22,21 @@ for(int i = 0; i<x.length();i++){
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
-String x.substring(1,9);			
+			String c = x.substring(i, i+1);
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-}
+			if(i % 2 == 0){
+				c = c.toUpperCase();
+			}else{
+				c = c.toLowerCase();
+			}
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
-
+goofyName+=c;
+}
 		
 		// 7. Use pop-up to show user their Goofy name
-
+JOptionPane.showMessageDialog(null,"Your goofy name is " + goofyName);
 	}
 }
 
--- src/algorithms/GuessingPi.java
+++ src/algorithms/GuessingPi.java
@@ -8,32 +8,41 @@ import javax.swing.JOptionPane;
 
 public class GuessingPi {
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
-
+String pi = "314159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195";
 		// 3. Print out the first 3 digits of Pi to the console.
 		//    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
 		//    Run your program to see how this works.
-
+System.out.println(pi.charAt(0));
 		// 9. If you want to give the user more than one chance to guess,
 		//    put a for loop around steps 4-8.
 
 			// 4. Create a for loop that will step through each digit of pi (steps 5-8)
 			//    NOTE: The number of digits in your string will be
 			//       yourStringVariable.length()
-
+for(int i=0;i < pi.length() ; i++ ){
 				// 5. Ask the user for the NEXT digit of pi.
-
+	String a = JOptionPane.showInputDialog("What is the next digit of pi?");
 				// 6. Compare the user's input to the next digit of your pi variable 
 				//    (look at step 3 for a clue).
 				//    HINT: use charAt(0) to get 1st char of user input String 
 
 					// 7. If they are correct, print out "correct".
-
+if(a.charAt(0) == pi.charAt(i)){
+	JOptionPane.showMessageDialog(null,"You are correct");
+}
 					// 8. If they are not, print out "incorrect" and tell them
-					//    to start over. Use 'break;' to break out of the loop.
+else{
+	JOptionPane.showMessageDialog(null,"You are incorrect");
+	JOptionPane.showMessageDialog(null,"Start over");
+	break;
+}					
+//    to start over. Use 'break;' to break out of the loop.
 
 }
-
-
+}
+}
